9b5a2ba9709a79b1d89518f178145f2fdccac86a
1sh1vam <shivam07.mt@gmail.com>
2022-10-11 19:32:36 +0530

    adding the header for home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,44 @@
-import { SafeAreaView, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
-import { useNavigation } from '@react-navigation/native'
+import { Image, SafeAreaView, Text, TextInput, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { ChevronDownIcon, UserIcon, MagnifyingGlassIcon, AdjustmentsVerticalIcon } from 'react-native-heroicons/outline';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
 
   useEffect(() => {
     navigation.setOptions({
-      headerShown: false
-    })
-  }, [navigation])
+      headerShown: false,
+    });
+  }, [navigation]);
   return (
-    <SafeAreaView>
-      <View>
-        <Text className="text-red-500">Deliveroo App</Text>
+    <SafeAreaView className="bg-white pt-5">
+      <View className="flex-row pb-3 items-center px-4 space-x-2">
+        <Image
+          source={{ uri: 'https://links.papareact.com/wru' }}
+          className="w-7 h-7 bg-gray-300 rounded-full p-4"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-xs text-gray-400">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="bg-gray-200 flex-row space-x-2 flex-1 p-3">
+          <MagnifyingGlassIcon size={20} color="gray" />
+          <TextInput placeholder='Restaurents and cuisines' />
+        </View>
+        <AdjustmentsVerticalIcon color="#00CCBB" />
       </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 export default HomeScreen;
+
