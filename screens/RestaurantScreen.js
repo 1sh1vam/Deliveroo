c0165eb76bf0911b91815ca3b77259569e36f463
1sh1vam <shivam07.mt@gmail.com>
2022-10-16 14:35:07 +0530

    restaurant details card

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -3,7 +3,7 @@ import React, { useLayoutEffect } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import { urlFor } from '../sanity';
 import { styled } from 'nativewind';
-import { ArrowLeftIcon } from 'react-native-heroicons/solid';
+import { ArrowLeftIcon, MapPinIcon, StarIcon } from 'react-native-heroicons/solid';
 
 const StyledPressable = styled(Pressable);
 
@@ -37,7 +37,24 @@ const RestaurantScreen = () => {
                 <ArrowLeftIcon size={20} color="#00CCBB" />
             </StyledPressable>
         </View>
-      <Text>RestaurantScreen</Text>
+      <View className="bg-white">
+        <View className="px-4 py-4">
+          <Text className="font-bold text-3xl">{title}</Text>
+          <View className="flex-row space-x-2 my-1">
+            <View className="flex-row items-center space-x-1">
+              <StarIcon size={22} color="green" opacity={0.5} />
+              <Text className="text-gray-500 text-xs">
+                <Text className="text-green-500">{rating}</Text> . {genre}
+              </Text>
+            </View>
+            <View className="flex-row items-center space-x-1">
+              <MapPinIcon size={22} color="gray" opacity={0.4} />
+              <Text className="text-gray-500 text-xs">Nearby . {address}</Text>
+            </View>
+          </View>
+          <Text className="text-gray-500 mt-2">{short_description}</Text>
+        </View>
+      </View>
     </ScrollView>
   )
 }
