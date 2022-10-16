d65ad51ce661cfab454d44a22f39e5fe735ef91e
1sh1vam <shivam07.mt@gmail.com>
2022-10-16 14:22:02 +0530

    restaurant header setup

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,11 +1,44 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import { Image, Pressable, ScrollView, Text, View } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { urlFor } from '../sanity';
+import { styled } from 'nativewind';
+import { ArrowLeftIcon } from 'react-native-heroicons/solid';
+
+const StyledPressable = styled(Pressable);
 
 const RestaurantScreen = () => {
+  const navigation = useNavigation();
+
+  const { params: {
+    id,
+    title,
+    imgUri,
+    rating,
+    genre,
+    address,
+    short_description,
+    dishes,
+    lat,
+    lng,
+  } } = useRoute();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+        headerShown: false
+    })
+  }, [])
+
   return (
-    <View>
+    <ScrollView>
+        <View className="relative">
+            <Image className="w-full h-56" source={{ uri: urlFor(imgUri).url() }} />
+            <StyledPressable className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full active:opacity-50">
+                <ArrowLeftIcon size={20} color="#00CCBB" />
+            </StyledPressable>
+        </View>
       <Text>RestaurantScreen</Text>
-    </View>
+    </ScrollView>
   )
 }
 
