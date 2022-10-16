f0f8b1f292a94d196d1df87f50f85d28b0e2336b
1sh1vam <shivam07.mt@gmail.com>
2022-10-16 18:09:05 +0530

    adding restaurant reducer

--- store/index.js
+++ store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import basketReducer from './reducers/basket';
+import restaurantReducer from './reducers/restaurant';
 
 export const store = configureStore({
    reducer: {
     basket: basketReducer,
+    restaurant: restaurantReducer,
    }
 })
