b8d39921a7aa4feafeb687711cc33dd207d61c65
1sh1vam <shivam07.mt@gmail.com>
2022-10-16 18:36:18 +0530

    showing all the items in basket

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,17 +1,37 @@
-import { Image, Pressable, SafeAreaView, Text, View } from 'react-native';
-import React from 'react';
+import {
+  Image,
+  Pressable,
+  SafeAreaView,
+  Text,
+  View,
+  ScrollView,
+} from 'react-native';
+import React, { useMemo } from 'react';
 import { useSelector } from 'react-redux';
-import { selectRestaurant } from '../store/reducers/restaurant';
 import { styled } from 'nativewind';
 import { XCircleIcon } from 'react-native-heroicons/solid';
 import { useNavigation } from '@react-navigation/native';
+import { selectRestaurant } from '../store/reducers/restaurant';
+import { selecteBasketItems } from '../store/reducers/basket';
+import { urlFor } from '../sanity';
 
 const StyledPressable = styled(Pressable);
 
 const BasketScreen = () => {
   const navigation = useNavigation();
   const restaurant = useSelector(selectRestaurant);
+  const items = useSelector(selecteBasketItems);
 
+  const groupedItems = useMemo(
+    () =>
+      items.reduce((results, item) => {
+        (results[item.id] = results[item.id] || []).push(item);
+        return results;
+      }, {}),
+    [items]
+  );
+
+  console.log('geer', groupedItems, items);
   return (
     <SafeAreaView className="flex-1">
       <View className="flex-1">
@@ -41,6 +61,23 @@ const BasketScreen = () => {
             <Text className="text-[#00CCBB]">Change</Text>
           </StyledPressable>
         </View>
+
+        <ScrollView className="divide-y divide-gray-200">
+          {Object.entries(groupedItems).map(([itemId, items]) => (
+            <View
+              key={itemId}
+              className="bg-white flex-row items-center space-x-3 px-5 py-2"
+            >
+              <Text className="text-[#00CCBB]">{items.length} x</Text>
+              <Image source={{ uri: urlFor(items[0].image).url() }} className="h-12 w-12 rounded-full" />
+              <Text className="flex-1">{items[0].name}</Text>
+              <Text className="text-gray-600">₹{items[0].price}</Text>
+              <StyledPressable className="active:opacity-50">
+                <Text className="text-[#00CCBB] text-xs">Remove</Text>
+              </StyledPressable>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </SafeAreaView>
   );
