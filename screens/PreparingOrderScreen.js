3880505b2b5c8d5f1c78819ba111da834ee8c16b
1sh1vam <shivam07.mt@gmail.com>
2022-10-16 19:12:08 +0530

    preparing order screen

---
+++ screens/PreparingOrderScreen.js
@@ -0,0 +1,16 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PreparingOrderScreen = () => {
+  return (
+    <SafeAreaView className="bg-[#00CCBB] flex-1">
+        <View className="justify-center items-center flex-1">
+            <Text>PreparingOrderScreen</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default PreparingOrderScreen
+
+const styles = StyleSheet.create({})
