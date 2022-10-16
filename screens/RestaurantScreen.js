98834d0602e4c32c94021ff35519f6236180afb7
1sh1vam <shivam07.mt@gmail.com>
2022-10-16 17:01:03 +0530

    adding padding to the dishe rows to unblock ui

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -7,6 +7,7 @@ import { ChevronRightIcon } from 'react-native-heroicons/outline';
 import { ArrowLeftIcon, MapPinIcon, StarIcon } from 'react-native-heroicons/solid';
 import { QuestionMarkCircleIcon } from 'react-native-heroicons/outline';
 import DishRow from '../components/DishRow';
+import BasketIcon from '../components/BasketIcon';
 
 const StyledPressable = styled(Pressable);
 
@@ -33,54 +34,57 @@ const RestaurantScreen = () => {
   }, [])
 
   return (
-    <ScrollView>
-      <View className="relative">
-          <Image className="w-full h-56" source={{ uri: urlFor(imgUri).url() }} />
-          <StyledPressable onPress={navigation.goBack} className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full active:opacity-50">
-              <ArrowLeftIcon size={20} color="#00CCBB" />
-          </StyledPressable>
-      </View>
-      <View className="bg-white">
-        <View className="px-4 py-4">
-          <Text className="font-bold text-3xl">{title}</Text>
-          <View className="flex-row space-x-2 my-1">
-            <View className="flex-row items-center space-x-1">
-              <StarIcon size={22} color="green" opacity={0.5} />
-              <Text className="text-gray-500 text-xs">
-                <Text className="text-green-500">{rating}</Text> . {genre}
-              </Text>
+    <>
+      <BasketIcon />
+      <ScrollView>
+        <View className="relative">
+            <Image className="w-full h-56" source={{ uri: urlFor(imgUri).url() }} />
+            <StyledPressable onPress={navigation.goBack} className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full active:opacity-50">
+                <ArrowLeftIcon size={20} color="#00CCBB" />
+            </StyledPressable>
+        </View>
+        <View className="bg-white">
+          <View className="px-4 py-4">
+            <Text className="font-bold text-3xl">{title}</Text>
+            <View className="flex-row space-x-2 my-1">
+              <View className="flex-row items-center space-x-1">
+                <StarIcon size={22} color="green" opacity={0.5} />
+                <Text className="text-gray-500 text-xs">
+                  <Text className="text-green-500">{rating}</Text> . {genre}
+                </Text>
+              </View>
+              <View className="flex-row items-center space-x-1">
+                <MapPinIcon size={22} color="gray" opacity={0.4} />
+                <Text className="text-gray-500 text-xs">Nearby . {address}</Text>
+              </View>
             </View>
-            <View className="flex-row items-center space-x-1">
-              <MapPinIcon size={22} color="gray" opacity={0.4} />
-              <Text className="text-gray-500 text-xs">Nearby . {address}</Text>
-            </View>
+            <Text className="text-gray-500 mt-2">{short_description}</Text>
           </View>
-          <Text className="text-gray-500 mt-2">{short_description}</Text>
+
+          <StyledPressable className="flex-row items-center p-4 space-x-2 border-y border-gray-300">
+            <QuestionMarkCircleIcon size={20} color="gray" opacity={0.6} />
+            <Text className="flex-1 font-semibold pl-2">Have a food allergy?</Text>
+            <ChevronRightIcon size={20} color="#00CCBB" />
+          </StyledPressable>
         </View>
 
-        <StyledPressable className="flex-row items-center p-4 space-x-2 border-y border-gray-300">
-          <QuestionMarkCircleIcon size={20} color="gray" opacity={0.6} />
-          <Text className="flex-1 font-semibold pl-2">Have a food allergy?</Text>
-          <ChevronRightIcon size={20} color="#00CCBB" />
-        </StyledPressable>
-      </View>
+        <View className="pb-36">
+          <Text className="font-bold text-xl px-4 pt-6 mb-3">Menu</Text>
 
-      <View>
-        <Text className="font-bold text-xl px-4 pt-6 mb-3">Menu</Text>
-
-        {/* Dish Arrows */}
-        {dishes.map((dish) => (
-          <DishRow
-            key={dish._id}
-            id={dish._id}
-            name={dish.name}
-            description={dish.short_description}
-            price={dish.price}
-            image={dish.image}
-          />
-        ))}
-      </View>
-    </ScrollView>
+          {/* Dish Arrows */}
+          {dishes.map((dish) => (
+            <DishRow
+              key={dish._id}
+              id={dish._id}
+              name={dish.name}
+              description={dish.short_description}
+              price={dish.price}
+              image={dish.image}
+            />
+          ))}
+        </View>
+      </ScrollView>
+    </>
   )
 }
 
