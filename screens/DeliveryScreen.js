460264be6f36061d45a198396976990cc175f6a6
1sh1vam <shivam07.mt@gmail.com>
2022-10-17 20:09:20 +0530

    delivery screen navigation header

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -1,9 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { styled } from 'nativewind';
+import { XMarkIcon } from 'react-native-heroicons/solid';
+
+const StyledPressable = styled(Pressable);
 
 const DeliveryScreen = () => {
   return (
-    <View>
+    <View className="flex-1 bg-[#00CCBB]">
+        <SafeAreaView className="z-50">
+            <View className="flex-row items-center justify-between p-5">
+                <StyledPressable className="active:opacity-50">
+                    <XMarkIcon color="white" size={30} />
+                </StyledPressable>
+                <Text className="text-white font-light text-lg">Order Help</Text>
+            </View>
+        </SafeAreaView>
       <Text>DeliveryScreen</Text>
     </View>
   )
